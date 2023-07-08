cde1f815fbed881699fd4db377d944f60a1f9f04
Awwi2 <l.behrendt@mtsl.de>
2023-07-08 12:09:27 +0200

    Added Framework for "Its on the House"

---
+++ GMTK - Projekt/Assets/Script/StatManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatManager : MonoBehaviour
+{
+    public int money = 1000;
+    public int moneyPerPerson = 50;
+    public int peoplePerDay;
+    public int risk = 0;
+    public int popularity = 10;
+    public int rent = 200;
+
+    public Text mon, mPP, r, pop, ren;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        peoplePerDay = popularity / 5;
+    }
+
+    public void executeDay()
+    {
+        peoplePerDay = popularity / 5;
+        money += moneyPerPerson * peoplePerDay;
+        money -= rent;
+        if(Random.Range(1,100) <= risk)
+        {
+            money -= Random.Range(100, 1000);
+            popularity -= Random.Range(10, 100);
+            pop.text = "Popularity: " + popularity;
+        }
+        mon.text = "Money: " + money;
+    }
+
+    public void tempMPPPlus()
+    {
+        moneyPerPerson += 10;
+        mPP.text = "Money Per Person: " + moneyPerPerson;
+    }
+
+    public void tempRiskPlus()
+    {
+        risk += 5;
+        r.text = "Risk: " + risk;
+    }
+
+    public void tempPopPlus()
+    {
+        popularity += 2;
+        pop.text = "Popularity: " + popularity;
+    }
+
+    public void tempRentPlus()
+    {
+        rent += 50;
+        ren.text = "Rent: " + rent;
+    }
+}
