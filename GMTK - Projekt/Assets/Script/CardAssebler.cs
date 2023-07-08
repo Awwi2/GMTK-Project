47bd241a856eddd48cd7032715df342de332dd85
Awwi2 <l.behrendt@mtsl.de>
2023-07-08 14:37:12 +0200

    Added Functionality to add cards

---
+++ GMTK - Projekt/Assets/Script/CardAssebler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class NewBehaviourScript : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public string effect1;
+    public string effect2;
+    public string effect3;
+
+    public Sprite image;
+
+    public int moneyModifier;
+    public int popularityModifier;
+    public int rentModifier;
+    public int riskModifier;
+    public int moneyPerPersonModifier;
+
+    public int rarity;
+}
