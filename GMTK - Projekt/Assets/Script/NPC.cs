84dbcab4a472bf6458554b07d90c341b9afa3af7
Tiefkuhlfach <tomschwrs@gmail.com>
2023-07-10 21:56:37 +0200

    Npc

--- GMTK - Projekt/Assets/Script/NPC.cs
+++ GMTK - Projekt/Assets/Script/NPC.cs
@@ -7,13 +7,10 @@ public class NPC : MonoBehaviour
     public float speed = 2f;
     public Rigidbody2D rb;
 
-    private void Start()
-    {
-        Physics2D.IgnoreCollision(GameObject.FindWithTag("Player").GetComponent<Collider2D>(), this.gameObject.GetComponent<Collider2D>());
-    }
     void Update()
     {
         rb.velocity = new Vector2(speed, rb.velocity.y);
+        Physics2D.IgnoreCollision(GameObject.FindWithTag("Player").GetComponent<Collider2D>(), this.gameObject.GetComponent<Collider2D>());
 
     }
     public void OnTriggerEnter2D(Collider2D collision)
