00be59db41513a325a6c0527a4d20c464ed2577f
Dudelord <85459353+LorOfTheDudes@users.noreply.github.com>
2023-07-11 16:25:50 +0200

    Audio is now Everywhere
    And it saves between Scenes and Plays

--- GMTK - Projekt/Assets/Script/PauseMenu.cs
+++ GMTK - Projekt/Assets/Script/PauseMenu.cs
@@ -6,11 +6,12 @@ using UnityEditor.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    
-    [SerializeField] UnityEngine.UI.Slider soundSlider;
+
     [SerializeField] AudioMixer masterVolume;
+    [SerializeField] UnityEngine.UI.Slider MasterSlider;
     [SerializeField] UnityEngine.UI.Slider SFXSlider;
     [SerializeField] UnityEngine.UI.Slider MusicSlider;
+
 
     public static bool paused = false;
 
@@ -18,23 +19,54 @@ public class PauseMenu : MonoBehaviour
 
     public void Awake()
     {
-        pauseMenuUi.SetActive(true);
-        SetMasterVolumeFromSlider();
-        SetMusicVolumeFromSlider();
-        SetSfxVolumeFromSlider();
-        pauseMenuUi.SetActive(false);
+<<<<<<< HEAD
+        float masterVol = PlayerPrefs.GetFloat("MasterVolume");
+        float musicVol = PlayerPrefs.GetFloat("MusicVolume");
+        float SFXVol = PlayerPrefs.GetFloat("SFXVolume");
+        masterVolume.SetFloat("MasterVolume", masterVol);
+        masterVolume.SetFloat("MusicVolume", musicVol);
+        masterVolume.SetFloat("SFXVolume", SFXVol);
+        soundSlider.value = (masterVol/40) + 1;
+        MusicSlider.value = (musicVol/40) +1;
+        SFXSlider.value = (SFXVol/40) + 1;
+        Debug.Log("Loaded Master Volume from PlayerPrefs:" + masterVol);
 
+
+=======
+        pauseMenuUi.SetActive(true);
+        GetAudioMixerVolume();
+        pauseMenuUi.SetActive(false);
+>>>>>>> 903bb0f9e6215672118dd0039b134d3beb3e578c
     }
 
+    public void GetAudioMixerVolume()
+    {
+        //Set the slider to the Value of the Master Mixer 
+        float masterValue;
+        bool masterMixer = masterVolume.GetFloat("MasterVolume", out masterValue); ;
+        MasterSlider.value = masterValue;
+        Debug.Log(masterValue);
+
+        float sfxValue;
+        bool sfxMixer = masterVolume.GetFloat("SFXVolume", out sfxValue); ;
+        SFXSlider.value = sfxValue;
+
+        float musicValue;
+        bool musicMixer = masterVolume.GetFloat("SFXVolume", out musicValue); ;
+        MusicSlider.value = musicValue;
+
+    }
     public void SetMasterVolumeFromSlider()
     {
-        float value = 0 - (1 - soundSlider.value) * 40;      
+        float value = 0 - (1 - MasterSlider.value) * 40;      
         //Mute the audio if the slider is all the way to the left
         if (value == -40)
         {
             value = -80;
         }
         masterVolume.SetFloat("MasterVolume", value);
+        PlayerPrefs.SetFloat("MasterVolume", value);
+        Debug.Log("Set Master Volume to:" + value + "And set Player Prefs to the same Value");
     }
     public void SetMusicVolumeFromSlider()
     {
@@ -45,6 +77,7 @@ public class PauseMenu : MonoBehaviour
             value = -80;
         }
         masterVolume.SetFloat("MusicVolume", value);
+        PlayerPrefs.SetFloat("MusicVolume", value);
 
     }
     public void SetSfxVolumeFromSlider()
@@ -56,6 +89,7 @@ public class PauseMenu : MonoBehaviour
             value = -80;
         }
         masterVolume.SetFloat("SFXVolume", value);
+        PlayerPrefs.SetFloat("SFXVolume", value);
 
     }
     void Update()
